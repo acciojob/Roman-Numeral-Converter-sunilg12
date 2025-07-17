cca6172dd569e561e0b9adebbb9745e56cab065d
Sunil Reddy Gaddam <gaddamsunilreddy20@gmail.com>
2025-07-17 20:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,13 +8,20 @@ function convertToRoman(num) {
       5:['V', 5], 
       6:['I', 1]
     };
-
+https://www.svgrepo.com/show/345221/three-dots.svg
   //your code here
+	let result="";
+	Object.values(obj).forEach(([symbol,value]) =>{
+		while(num>value){
+			result += symbol;
+			num -= value;
+		}
+	})
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-// console.log(convertToRoman(36));
+console.log(convertToRoman(36));
 
 
 
